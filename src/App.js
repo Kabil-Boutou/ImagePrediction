ac6085c8bce98ced9430ea846a41f309a399eee1
Kabil-Boutou <kabil.boutou@gmail.com>
2021-11-11 19:01:29 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import tiger from "./tiger.jpg";
-// Importing ml5.js as ml5
-import * as ml5 from "ml5";
+import { useState, useEffect } from 'react'
+import * as ml5 from 'ml5'
 
-class App extends Component {
-  state = {
-    predictions: []  // Set the empty array predictions state
-  }
+import './App.css'
+import tiger from './peakpx.jpeg'
 
-  setPredictions = (pred) => {
-    // Set the prediction state with the model predictions
-    this.setState({
-      predictions: pred
-    });
-  }
+export default function Ml5js() {
+  const [predictions, setPredictions] = useState([])
 
-  classifyImg = () => {
+  const classifyImg = () => {
     // Initialize the Image Classifier method with MobileNet
-    const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+    const classifier = ml5.imageClassifier('MobileNet', modelLoaded)
     // When the model is loaded
     function modelLoaded() {
-      console.log('Model Loaded!');
+      console.log('Model Loaded!')
     }
     // Put the image to classify inside a variable
-    const image = document.getElementById('image');
+    const image = document.getElementById('image')
     // Make a prediction with a selected image
-    classifier.predict(image, 5, function(err, results) {
-    // Return the results
-      return results;
-    })
-      .then((results) => {
-        // Set the predictions in the state
-        this.setPredictions(results)
-      })
-  }
+    classifier.classify(image, gotResult)
 
-  componentDidMount(){
-    // once the component has mount, start the classification
-    this.classifyImg();
-  }
-
-  render() {
-    // First set the predictions to a default value while loading
-    let predictions = (<div className="loader"></div>);
-    // Map over the predictions and return each prediction with probability
-    if(this.state.predictions.length > 0){
-      predictions = this.state.predictions.map((pred, i) => {
-        let { className, probability } = pred;
-        // round the probability with 2 decimal
-        probability = Math.floor(probability * 10000) / 100 + "%";
-        return (
-          <div key={ i + "" }>{ i+1 }. Prediction: { className } at { probability } </div>
-        )
-      })
+    // A function to run when we get any errors and the results
+    function gotResult(error, results) {
+      // Display error in the console
+      if (error) {
+        console.error(error)
+      } else {
+        // The results are in an array ordered by confidence.
+        console.log(results)
+        //createDiv(`Label: ${results[0].label}`)
+        //createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`)
+        setPredictions(results)
+      }
     }
-
-    return (
-      <div className="App">
-      <h1>Image classification with ML5.js</h1>
-      <img src={ tiger } id="image" width="400" alt="" />
-      { predictions }
-      </div>
-    );
   }
-}
 
-export default App;
+  useEffect(() => {
+    classifyImg()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Image classification with ML5.js</h1>
+      <img src={tiger} id="image" width="400" alt="" />
+      {predictions.length > 0 ? (
+        predictions.map((pred, i) => {
+          let { label, confidence } = pred
+          // round the probability with 2 decimal
+          confidence = Math.floor(confidence * 10000) / 100 + '%'
+          return (
+            <div key={i + ''}>
+              {i + 1}. Prediction: {label} at {confidence}{' '}
+            </div>
+          )
+        })
+      ) : (
+        <div className="loader"></div>
+      )}
+    </div>
+  )
+}
